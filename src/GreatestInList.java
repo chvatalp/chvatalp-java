831354635f74cd920e95fa711fa3247119b91144
chvatalp <56312836+chvatalp@users.noreply.github.com>
2020-02-21 17:26:12 +0100

    Added GreatestInList.java

--- src/GreatestInList.java
+++ src/GreatestInList.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GreatestInList {
     public static void main(String[] args) {
-        
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Integer> numbersList = new ArrayList<>();
+
+        while (true) {
+            int number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            }
+            numbersList.add(number);
+        }
+
+        int greatestNumber = numbersList.get(0);
+        for (int num : numbersList) {
+            if (num > greatestNumber) {
+                greatestNumber = num;
+            }
+        }
+
+        System.out.println("The greatest number is " + greatestNumber);
     }
 }
